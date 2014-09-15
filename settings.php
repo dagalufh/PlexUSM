985d84c984406a1d4eb901c7b2bbecfe5221fcd3
Mikael <dagalufh@gmail.com>
2014-09-15 01:47:09 -0400

    Moved settings to seperate file

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+// Settings
+$Username = "l-admin";
+$PathToPlexMediaFolder = "C:/users/".$Username."/AppData/Local/Plex Media Server/Media/Localhost/";
+$PathToPlexDatabase = "c:/users/".$Username."/AppData/Local/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db";
+?>
