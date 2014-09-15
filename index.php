c6987813b37f0a0a9a0305ad2f64e2b85617698e
Mikael <dagalufh@gmail.com>
2014-09-15 01:47:31 -0400

    Removed settings, added include instead.

--- index.php
+++ index.php
@@ -28,11 +28,8 @@ session_start();
 
 include("ListDir.php");
 include("classes.php");
+include("settings.php");
 
-// Settings
-$Username = "l-admin";
-$PathToPlexMediaFolder = "C:/users/".$Username."/AppData/Local/Plex Media Server/Media/Localhost/";
-$PathToPlexDatabase = "c:/users/".$Username."/AppData/Local/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db";
 $db = new SQLite3($PathToPlexDatabase);
 /**
  * Define some variables used.
