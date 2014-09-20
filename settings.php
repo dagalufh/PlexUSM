a0059e12adb58d7e9c0df4d0491d2a94945a1f8e
Mikael Aspehed <dagalufh@gmail.com>
2014-09-20 22:12:49 +0200

    Added  variable.

--- settings.php
+++ settings.php
@@ -1,6 +1,12 @@
 <?php
-// Settings
+/**
+ * Settings
+ * The PathToPlexMediaFolder will be overwritten if the Devtools.bundle is available in your plugins directory as the script fetches this information from that plugin instead.
+ */
 $Username = "l-admin";
 $PathToPlexMediaFolder = "C:/users/".$Username."/AppData/Local/Plex Media Server/Media/Localhost/";
-$PathToPlexDatabase = "c:/users/".$Username."/AppData/Local/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db";
+$AppendPathWith = "/Media/Localhost/";
+
+$Server = "http://127.0.0.1:32400";
+$ItemsPerPage = 5;
 ?>
