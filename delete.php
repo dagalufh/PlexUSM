011d52ea6b985ed7670bdc9ba8dcf0ba732a8935
Mikael <dagalufh@gmail.com>
2014-09-25 01:59:00 -0400

    Added some logging depending on sucessfull removal or not. Changed the way the file reloads parent window after completion.

--- delete.php
+++ delete.php
@@ -1,14 +1,29 @@
 <?php
-// Deletes the provided files.
+/**
+ * The purpose of this file is to delete the selected files.
+ *
+ *
+ * Include the file with the logfunction
+ */
+include("functions.php");
 
+/**
+ * Go through the selected checkboxes and remove the files.
+ */
 foreach ($_POST['Subtitle'] as $Subtitle) {
 	if(file_exists($Subtitle)) {
-		unlink($Subtitle);	
+		if (unlink($Subtitle)) {
+			USMLog("info","Successfully deleted file:" . $Subtitle . "\n");
+		} else {
+			USMLog("error","Failed deleting file:" . $Subtitle . "\n");
+		}
 	}
 }
-// After work is done, reload the original window.
+
+/**
+ * After completion, reload the page.
+ */
 ?>
 <script>
-
-	window.parent.location = document.referrer;
+	window.parent.location.href = windows.parent.location.href;
 </script>
