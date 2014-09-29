72f78c1415b6b2c6d5c97e61a5002d132267c771
Mikael Aspehed <dagalufh@gmail.com>
2014-09-29 19:57:57 +0200

    Modified to use DevTools for reading.

--- ReadFile.php
+++ ReadFile.php
@@ -1,30 +1,27 @@
+<?php
+include("settings.php");
+include("functions.php");
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
 	<title>Plex Unofficial Subtitle Manager</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<?php
-if(is_dir($_GET['FileToOpen'])) {
-	?>
-  		<div id="headline">An Error Has Occured</div>
-		<div id="mainText">
-			<p>The file sent to be open is in fact a directory!</p>
+	</head>
+	<body>
+		<?php
+		$CorrectedFilename = preg_replace("/ /", "%20", $_GET['FileToOpen']);
+		if(strpos($CorrectedFilename,"file://")!==false) {
+			$CorrectedFilename = substr($CorrectedFilename,7);
+		}
+		if(exists($CorrectedFilename) !== false) {
+			$ReturnValue = file_get_contents($Server . "/utils/devtools?Func=ShowSRT&Secret=" . $DevToolsSecret . "&FileName=".$CorrectedFilename);
+		}
+		?>
+		<div id="headline" ><?php echo $_GET['FileToOpen']?></div>
+		<div id="mainText" name="">
+			<textarea id="FileContent" name="FileContent" class="EditText" wrap="off" readonly><?php echo htmlspecialchars($ReturnValue)?></textarea>
 		</div>
-	<?php
-die();
-}
-
-$fp = fopen ($_GET['FileToOpen'], "r");
-$bytes = filesize($_GET['FileToOpen']);
-$buffer = fread($fp, $bytes);
-fclose ($fp);
-?>
-	  		<div id="headline" ><?php echo $_GET['FileToOpen']?> (<?php echo round($bytes/1000,2)?>KB)</div>
-			<div id="mainText" name="">
-				<textarea id="FileContent" name="FileContent" class="EditText" wrap="off" readonly><?php echo htmlspecialchars($buffer)?></textarea>
-			</div>
-</body>
+	</body>
 </html>
